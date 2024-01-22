ccdc1182dea17a44671d0afbb9a13f35a0d86fed
David Kurzmann <david.g.kurzmann@gmail.com>
2024-01-22 14:00:29 +0100

    Feature: Navigation is visible only when scrollling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
 import { Transition } from '@headlessui/react'
+import React, { useEffect, useState } from 'react'
 
 export default function Header() {
   const navStartPageID = 'navStartPageID'
   const navGuestBookID = 'navGuestBookID'
+  let [isNavOpen, setIsNavOpen] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setIsNavOpen(true)
+      } else {
+        setIsNavOpen(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <div
       id="headerID"
       className="pointer-events-none fixed top-6 z-30 grid w-full grid-cols-[1fr,min(640px,100%),1fr] px-4">
       <Transition
         className="shadow-surface-glass pointer-events-auto col-start-2 -mx-px rounded-2xl bg-gray-800/95 px-4 py-2.5 backdrop-blur will-change-transform [@supports(backdrop-filter:blur(0px))]:bg-stone-700/[25%]"
-        show={true}
+        show={isNavOpen}
         enter="transition duration-100 ease-in-out"
         enterFrom="opacity-0 scale-90"
         enterTo="opacity-100 scale-100"
